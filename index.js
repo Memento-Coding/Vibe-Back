8ac007c338c3aac17b0a193deb5c3f44ecf92ced
Juan Hoyos <juanhoyos1347@gmail.com>
2023-06-14 00:48:21 -0500

    structure

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+app.listen(process.env.PORT || 3030, () => {
+    console.log("Server running in port " + process.env.PORT);
+})
+
+
